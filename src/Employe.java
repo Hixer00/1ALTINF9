50b47b1d756a9587fb8317ce228303c7007a77b3
Hamza Baktache <hamza.baktache@alight.eu>
2023-03-25 01:17:39 +0100

    First Commit

---
+++ src/Employe.java
@@ -0,0 +1,87 @@
+import java.util.Objects;
+public class Employe {
+    private String cin;
+    private String matricule;
+    private String nom;
+    private String prenom;
+    private String departementId;
+
+    public Employe() {
+    }
+
+    public Employe(String cin, String matricule, String nom, String prenom, String departementId) {
+        this.cin = cin;
+        this.matricule = matricule;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.departementId = departementId;
+    }
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getDepartementId() {
+        return departementId;
+    }
+
+    public void setDepartementId(String departementId) {
+        this.departementId = departementId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employe{" +
+                "cin='" + cin + '\'' +
+                ", matricule='" + matricule + '\'' +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", departementId='" + departementId + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employe employe = (Employe) o;
+        return Objects.equals(cin, employe.cin) &&
+                Objects.equals(matricule, employe.matricule) &&
+                Objects.equals(nom, employe.nom) &&
+                Objects.equals(prenom, employe.prenom) &&
+                Objects.equals(departementId, employe.departementId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cin, matricule, nom, prenom, departementId);
+    }
+}
---
+++ src/SocieteTreeMap.java
@@ -0,0 +1,69 @@
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class SocieteTreeMap implements InterfaceSociete {
+
+    private Map<String, Employe> employes;
+    private Map<String, Departement> departements;
+
+    public SocieteTreeMap() {
+        this.employes = new TreeMap<>();
+        this.departements = new TreeMap<>();
+    }
+
+    @Override
+    public void ajouterEmployeDepartement(Employe e, Departement d) {
+        this.employes.put(e.getCin(), e);
+        this.departements.put(d.getId(), d);
+    }
+
+    @Override
+    public void supprimerEmploye(Employe e) {
+        this.employes.remove(e.getCin());
+    }
+
+    @Override
+    public void afficherLesEmployesLeursDepartements() {
+        Set<String> cinList = this.employes.keySet();
+        for (String cin : cinList) {
+            Employe e = this.employes.get(cin);
+            Departement d = this.departements.get(e.getDepartementId());
+            System.out.println(e.toString() + " travaille au département " + d.getNom());
+        }
+    }
+
+    @Override
+    public void afficherLesEmployes() {
+        Set<String> cinList = this.employes.keySet();
+        for (String cin : cinList) {
+            Employe e = this.employes.get(cin);
+            System.out.println(e.toString());
+        }
+    }
+
+    @Override
+    public void afficherLesDepartements() {
+        Set<String> idList = this.departements.keySet();
+        for (String id : idList) {
+            Departement d = this.departements.get(id);
+            System.out.println(d.toString());
+        }
+    }
+
+    @Override
+    public void afficherDepartement(Employe e) {
+        Departement d = this.departements.get(e.getDepartementId());
+        System.out.println(e.toString() + " travaille au département " + d.getNom());
+    }
+
+    @Override
+    public boolean rechercherEmploye(Employe e) {
+        return this.employes.containsValue(e);
+    }
+
+    @Override
+    public boolean rechercherDepartement(Departement d) {
+        return this.departements.containsValue(d);
+    }
+}
